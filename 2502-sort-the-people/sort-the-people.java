872e74bee87cf74d0ded0ee82c217118d7a3419d
Kartik Itankar <kartikitankar7615@gmail.com>
2026-02-03 22:05:02 +0530

    Time: 26 ms (13.46%) | Memory: 47.3 MB (57.14%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = heights.length;
+
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(heights[j] < heights[j+1]){
+                    int tempH = heights[j];
+                    heights[j] = heights[j+1];
+                    heights[j+1] = tempH;
+
+                    String tempN = names[j];
+                    names[j] = names[j+1];
+                    names[j+1] = tempN;
+                }
+            }
+        }
+        return names;
+    }
+}
